c8887f7e1c15d58af4160c8a2d346c33faa7d47b
PlasticTong <72696179+PlasticTong@users.noreply.github.com>
2023-09-21 13:29:50 +0800

    修复跳数算法和导入规则

--- app.py
+++ app.py
@@ -23,15 +23,20 @@ def read():
     name = data['name']
     data_res = []
     index = 1
-    with open("./data/" + name, 'r') as file:
+    with open("./data/" + name, 'r',encoding='utf-8-sig') as file:
         csv_reader = csv.reader(file)
         header = next(csv_reader)  # 读取第一行作为表头
+
+        # 获取标题所在列的索引
+        source_index = header.index('source')
+        target_index = header.index('target')
+        time_index = header.index('time')
         for row in csv_reader:
             data_res.append({
                 'id': index,
-                'source': row[0],
-                'target': row[1],
-                'time': row[2],
+                'source': row[source_index],
+                'target': row[target_index],
+                'time': row[time_index],
             })
             index = index + 1
     return data_res
@@ -54,7 +59,7 @@ def searchFromIPS(data, user):
     for data_mes in data:
         for ip in Ips:
             parts = ip.split(".")
-            ip_result = ".".join(parts[:3])+'.'
+            ip_result = ".".join(parts[:3]) + '.'
             if ip_result in data_mes['source'] or ip_result in data_mes['target']:
                 res.append(data_mes)
     return res
@@ -84,10 +89,12 @@ def searchHopOnce(data, mes_hop, n, start, end):
                 res.append(index['id'])
             for data_index in data:
                 if start <= data_index['timesecond'] <= end:
-                    if data_index['timesecond'] < mes_index['timesecond'] and mes_index['source'] == data_index['target']:
+                    if data_index['timesecond'] < mes_index['timesecond'] and mes_index['source'] == data_index[
+                        'target']:
                         res.append(data_index['id'])
                         temp.append(data_index['id'])
-                    if data_index['timesecond'] > mes_index['timesecond'] and mes_index['target'] == data_index['source']:
+                    if data_index['timesecond'] > mes_index['timesecond'] and mes_index['target'] == data_index[
+                        'source']:
                         res.append(data_index['id'])
                         temp.append(data_index['id'])
         mes = list(set(temp))
@@ -98,6 +105,70 @@ def searchHopOnce(data, mes_hop, n, start, end):
     return res_final
 
 
+@app.route('/mesBE/check_ip', methods=['POST'])
+def check_ip():
+    data_json = request.get_json()
+    data = data_json["data"]
+    form = data_json["form"]
+    ips = form['user'].split(";")
+    hop = int(form['hop'])
+    start_time = form["timestart"]
+    end_time = form["timeend"]
+    df = pd.DataFrame(data, index=None)
+    df = df[(df.timesecond >= start_time) & (df.timesecond <= end_time)]
+    new_df = pd.DataFrame()
+    print(df)
+    for ip_ in ips:
+        up, down = [ip_], [ip_]
+        for i in range(1, hop + 1):
+            if len(up) != 0:
+                for ip in up:
+                    if i == 1:
+                        df_1 = df[df.target == ip]
+                        if len(df_1) != 0:
+                            up = df_1.loc[:, ['source', 'timesecond']].values
+                            new_df = pd.concat([new_df, df_1], axis=0)
+                        else:
+                            up = []
+                    else:
+                        df_1 = df[(df.target == ip[0]) & (df.timesecond < ip[1])]
+                        if len(df_1) != 0:
+                            up = df_1.loc[:, ['source', 'timesecond']].values
+                            new_df = pd.concat([new_df, df_1], axis=0)
+                        else:
+                            up = []
+            if len(down) != 0:
+                for ip in down:
+                    if i == 1:
+                        df_2 = df[df.source == ip]
+                        if len(df_2) != 0:
+                            down = df_2.loc[:, ['source', 'timesecond']].values
+                            new_df = pd.concat([new_df, df_2], axis=0)
+                        else:
+                            down = []
+                    else:
+                        df_2 = df[(df.target == ip[0]) & (df.timesecond < ip[1])]
+                        if len(df_2) != 0:
+                            up = df_1.loc[:, ['source', 'timesecond']].values
+                            new_df = pd.concat([new_df, df_2], axis=0)
+                        else:
+                            down = []
+            # for ip in up:
+            #     df_1 = df[df.target == ip]
+            #     up = df_1.source.tolist()
+            #     print(up)
+            #     new_df = pd.concat([new_df, df_1], axis=0)
+            # for ip in down:
+            #     df_2 = df[df.source == ip]
+            #     down = df_2.target.tolist()
+            #     new_df = pd.concat([new_df, df_2], axis=0)
+    new_df = new_df.drop_duplicates(keep='first')
+    print(new_df)
+    dd = new_df.sort_values('id').to_dict("records")
+    print(dd)
+    return dd
+
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port='5002')
+    app.run(host='0.0.0.0', port='5003')
